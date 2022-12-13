90dace231ec5fcc307b00136bdba38f1067c23c3
AliRezabeydaghi <akbry9461@gmail.com>
2022-12-13 20:16:49 +0330

    Edit Code

--- todo.js
+++ todo.js
@@ -12,9 +12,9 @@ try {
 // set default value if todos == null
 if (!todos) {
 	todos = [
-		"lerning js",
-		"Watch videos",
-		"be strong",
+	    {content :"lerning js", status : true},
+	    {content :"Watch videos", status : false},
+	    {content :"be strong", status : true},
 	]
 	localStorage.setItem("todos", JSON.stringify(todos))
 }
@@ -27,9 +27,10 @@ function createTodo(todos) {
 		let li = document.createElement("li")
 		li.className = "list-group-item"
 		let content= document.createElement("span")
-		content.textContent = todo
+		content.textContent = todo.content
+		content.style.textDecoration = todo.status ? 'initial' : 'line-through'
 		let deleteBtn = document.createElement("img")
-		deleteBtn.src = "media/delete.png"
+		deleteBtn.src = "media/icon_trash.png"
 		deleteBtn.className = "float-right"
 		li.append(content)
 		li.append(deleteBtn)
@@ -41,6 +42,58 @@ function createTodo(todos) {
 			localStorage.setItem("todos", JSON.stringify(todos))
 			createTodo(todos)
 		})
+		content.addEventListener("click", e =>{
+			todos[index].status = !todos[index].status
+			localStorage.setItem("todos", JSON.stringify(todos))
+			createTodo(todos)
+		})
 	});
 }
 createTodo(todos)
+
+
+let clicks = document.querySelector('#clicks')
+let forms = document.querySelector('#forms')
+
+
+Array.from(clicks.children).forEach(action => {
+	if (action.dataset.action == "add") {
+		action.addEventListener("click", e => {
+			forms.innerHTML = `
+				<form id="add">
+					<input class="form-control" name="add" placeholder="Add todo ..">
+				</form>
+			`
+			createTodo(todos)
+			let add = document.querySelector('#add')
+			add.addEventListener("submit", e =>{
+				e.preventDefault()
+				if (add.add.value) {
+					todos.push({content :add.add.value , status:true})
+					localStorage.setItem("todos", JSON.stringify(todos))
+			        createTodo(todos)
+				}
+			})
+		})
+	} else if (action.dataset.action == "search") {
+		action.addEventListener("click", e => {
+			forms.innerHTML = `
+				<form id="search">
+					<input class="form-control" name="search" placeholder="Search todos ..">
+				</form>
+			`
+			let search = document.querySelector("#search")
+			search.addEventListener("keyup", e =>{
+				e.preventDefault()
+				if (search.search.value) {
+					let filterd_todos = todos.filter(
+						todo => todo.content.toLowerCase().includes(search.search.value.toLowerCase())
+						)
+					createTodo(filterd_todos)
+				}else {
+					createTodo(todos)
+				}
+			})
+		})
+	}
+})
